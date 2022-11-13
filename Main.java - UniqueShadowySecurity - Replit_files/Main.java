49821b5dcfc60c666b193343ad54e1d1c63f06fc
Meghana-kalwad <meghnarkalawad861.com>
2022-11-13 09:34:40 -0800

    added

---
+++ Main.java - UniqueShadowySecurity - Replit_files/Main.java
@@ -0,0 +1,15 @@
+
+class Main {
+  public static void main(String[] args) {
+
+  String s="helloworld";
+  
+    for(int i=s.length(); i>0; i--){
+      System.out.println(s.charAt(i));
+        
+
+      
+    
+    
+  }
+}
